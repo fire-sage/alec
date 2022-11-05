d1a57cdebd23bb0ff5fba91b046f6326a0c8d80a
fire-sage <117492603+fire-sage@users.noreply.github.com>
2022-11-04 20:54:57 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Jake":return img`
+. . . . b b b b b b b b . . . . 
+. . . . b b b b b b b b . . . . 
+. . 5 5 5 b 5 b 5 b 5 b . . . . 
+. . 5 5 f f 5 b 5 d 5 5 . . . . 
+. . 5 5 1 f 5 d 5 d d d 5 . . . 
+. . . . f f d d d d d d 5 . . . 
+. . . . d d d d d d d d 5 . . . 
+. . b b b b b b b b b b b b . . 
+. . . b . b b b b b b . b . . . 
+. . . b . b b b b b b . b . . . 
+. . b b . b b b b b b . b b . . 
+. b b b . b b b b b b . b b b . 
+. b b b . b b . . b b . b b b . 
+. . d d . b b . . b b . d d . . 
+. . . . . b b . . b b . . . . . 
+. . . . . d d . . d d . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
